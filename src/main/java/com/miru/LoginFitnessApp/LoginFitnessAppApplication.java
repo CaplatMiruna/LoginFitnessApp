7e1e409bc1965ff2ea666c8fc8b6025561908606
CaplatMiruna <mctxy05@gmail.com>
2024-04-22 09:11:27 +0300

    Spring Boot Init

---
+++ src/main/java/com/miru/LoginFitnessApp/LoginFitnessAppApplication.java
@@ -0,0 +1,13 @@
+package com.miru.LoginFitnessApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoginFitnessAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoginFitnessAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/miru/LoginFitnessApp/LoginFitnessAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.miru.LoginFitnessApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoginFitnessAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
